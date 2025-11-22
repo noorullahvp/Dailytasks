bad545859729f052603f9fae5dc910138a4ca246
muhammed noorullah <muhammednoorullahvp@gmail.com>
2025-11-22 12:41:14 +0530

    Added array and stringmethods projects

---
+++ stringmethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string name = "Muhammed,Noorullah";
+        string fullName = " muhammed Noorullah";
+        string[] names = name.Split(',');
+        bool check = name.Equals(fullName);
+        int length = name.Length;
+        {
+            Console.WriteLine(length);
+            Console.WriteLine(name.ToUpper());
+            Console.WriteLine(name.ToLower());
+            Console.WriteLine(fullName.Trim());
+            Console.WriteLine(fullName.Contains("Noor"));
+            Console.WriteLine(name.Substring(0, 5));
+            Console.WriteLine(name.Substring(1, 9));
+            Console.WriteLine(name.Substring(5));
+            Console.WriteLine(name.Replace("Muhammed", "Safa"));
+            Console.WriteLine(string.Join("|",names));
+            Console.WriteLine(name.IndexOf("N"));
+            Console.WriteLine(name.Remove(4, 6));
+            Console.WriteLine(name.Insert(9,"vp"));
+            Console.WriteLine(check);
+
+        }
+    }
+}
